2957a00a698bc88735a9de026e834670fab2a0e0
Neuro Nada Academy <powermovement.id@gmail.com>
2026-03-29 19:48:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ if st.button("Mulai Pemetaan Internal", type="primary"):
             3: "http://lynk.id/neuronada/0rd6gr7nlzxp/checkout",
             4: "http://lynk.id/neuronada/elp83loeyggg/checkout",
             5: "http://lynk.id/neuronada/wne9p4q1l3d9/checkout",
-            6: "https://lynk.id/username_lu/produk-angka-6",
+            6: "http://lynk.id/neuronada/nm840y6nlo21/checkout",
             7: "https://lynk.id/username_lu/produk-angka-7",
             8: "https://lynk.id/username_lu/produk-angka-8",
             9: "https://lynk.id/username_lu/produk-angka-9"
